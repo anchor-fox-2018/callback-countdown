f15c5011150f26e8d5dc3a330e8a8388c89533b3
days88 <yayahidayatullah@rocketmail.com>
2018-09-13 19:46:08 +0800

    Release 1

--- controllers.js
+++ controllers.js
@@ -2,8 +2,38 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
-};
+  let stopTimer =  setInterval(function(){
+      let menit = Math.floor(seconds/60);
+      let detik = seconds % 60;
+      seconds--
+      if(menit < 10 && detik < 10){
+        displayCountdown(`0${menit} : 0${detik}`);
+      }else if(menit < 10 && detik > 10){
+        displayCountdown(`0${menit} : ${detik}`);
+      }else if(menit > 10 && detik > 10){
+        displayCountdown(`${menit} : ${detik}`);
+      }else if(menit > 10 && detik < 10){
+        displayCountdown(`${menit} : 0${detik}`);
+      }else if(menit === 10){
+        displayCountdown(`${menit} : 0${detik}`);
+      }else if(menit < 10){
+        displayCountdown(`0${menit} : ${detik}`);
+      }else if(detik < 10){
+        displayCountdown(`${menit} : ${detik}`);
+      }else{
+        displayCountdown(`${menit} : ${detik-1}`);
+      }
+      if(seconds < -1){
+        //displayCountdown(`0${menit} : 0${detik}`);
+        clearInterval(stopTimer);
+        var clear = require('clear')
+        clear()
+      }
+    },1000)
+  }
+// function Countdown(seconds){
 
+// }
 module.exports = {
   timer
 };
